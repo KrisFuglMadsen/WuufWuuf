fee870bec5b6c2b373a553d5f9e10add9b96582c
KrisFuglMadsen <kfma54102@edu.ucl.dk>
2023-11-09 10:03:51 +0100

    Update DB first step

--- KennelCarolinekilde/Views/DBView.xaml.cs
+++ KennelCarolinekilde/Views/DBView.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using ExcelDataReader;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +28,88 @@ namespace KennelCarolinekilde.Views
         public DBView()
         {
             InitializeComponent();
+        }
+
+        DataTable dataTable;
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtFileName.Text = openFileDialog.FileName;
+
+                using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                {
+                    System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                    using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        //read the excel data and configure to treat the first row as headers
+                        DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration
+                        {
+                            ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                            {
+                                UseHeaderRow = true //Treat the first row as headers
+                            }
+                        });
+
+                        if (result.Tables.Count > 0)
+                        {
+                            dataTable = result.Tables[0]; // Assuming the data is in the first (and only) sheet
+
+                            ExcelFilePresenter.ItemsSource = dataTable.DefaultView;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No data found in the Excel file.");
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void btnUpdateDB_Click(object sender, RoutedEventArgs e)
+        {
+
+            // call a method in the ViewModel Layer ---> wich calls a method in Model layer to connect to the DB. 
+
+            string connectionString = "Server = 10.56.8.36; Database = DB_F23_TEAM_02; User ID = DB_F23_TEAM_02; Password = TEAMDB_DB_02; TrustServerCertificate = true;"; ;
+
+            try
+            {
+                if (dataTable != null)
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+
+                        using (SqlCommand command = new SqlCommand("dbo.InsertDogs", conn))
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+
+                            SqlParameter dogsParamter = command.Parameters.AddWithValue("@Dogs", dataTable);
+                            dogsParamter.SqlDbType = SqlDbType.Structured;
+                            dogsParamter.TypeName = "dbo.DogType";
+
+                            command.ExecuteNonQuery();
+
+                            MessageBox.Show(" Data Inserted Into the DB");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No Data to update");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+
+            }
+
+
         }
     }
 }
